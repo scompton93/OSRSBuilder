4384a976b5b818215c8f3f81073eba1c721e67be
Sean <scompton93@users.noreply.github.com>
2023-02-15 19:49:19 +1100

    Add project files.

---
+++ ObfuscationMapGenerator/ObfuscatedType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObfuscationMapGenerator
+{
+    internal class ObfuscatedType
+    {
+        public ObfuscatedType() { ObfuscatedMethods = new List<ObfuscatedMethod>(); }
+        internal string DeobfuscatedName { get; set; }
+        internal string ObfuscatedName { get; set; }
+        internal string Implements { get; set; }
+        internal List<ObfuscatedMethod> ObfuscatedMethods { get; set; }
+
+    }
+
+    internal class ObfuscatedMethod
+    {
+        public ObfuscatedMethod() { ObfuscatedSignature = new List<String>(); }
+        internal string DeobfuscatedName { get; set; }
+        internal string ObfuscatedName { get; set; }
+        internal List<String> ObfuscatedSignature { get; set; }
+        internal string Export { get; set; }
+    }
+}
---
+++ ObfuscationMapGenerator/Program.cs
@@ -0,0 +1,96 @@
+﻿using Mono.Cecil;
+
+namespace ObfuscationMapGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var module = ModuleDefinition.ReadModule(@"C:\Users\x\Desktop\osrs-211.dll");
+
+            var obfuscatedTypes = new List<ObfuscatedType>();
+
+            foreach (
+                var type in module.Types.Where(t => t.HasCustomAttributes && t.Name == "GameEngine")
+            )
+            {
+                if (type.CustomAttributes == null)
+                    continue;
+                var obfuscatedType = new ObfuscatedType();
+                obfuscatedType.DeobfuscatedName = type.Name;
+                foreach (var customAttribute in type.CustomAttributes)
+                {
+                    if (customAttribute.AttributeType.Name == "ObfuscatedNameAttribute")
+                    {
+                        CustomAttributeArgument[] constructorArguments = (CustomAttributeArgument[])
+                            customAttribute.ConstructorArguments.First().Value;
+
+                        obfuscatedType.ObfuscatedName = (string)
+                            constructorArguments[3].Value;
+                    }
+                    else if (
+                        customAttribute.AttributeType.FullName
+                        == "net.runelite.mapping.ImplementsAttribute"
+                    )
+                    {
+                        CustomAttributeArgument[] constructorArguments = (CustomAttributeArgument[])
+                            customAttribute.ConstructorArguments.First().Value;
+
+                        obfuscatedType.Implements = (string)
+                            constructorArguments[3].Value;
+                    }
+                }
+                foreach (var method in type.Methods)
+                {
+                    if (method.CustomAttributes == null)
+                        continue;
+                    var obfuscatedMethod = new ObfuscatedMethod();
+                    obfuscatedMethod.DeobfuscatedName = method.Name;
+                    foreach (var customAttribute in method.CustomAttributes)
+                    {
+                        if (customAttribute.AttributeType.Name == "ObfuscatedNameAttribute")
+                        {
+                            CustomAttributeArgument[] constructorArguments =
+                                (CustomAttributeArgument[])
+                                    customAttribute.ConstructorArguments.First().Value;
+
+                            obfuscatedMethod.ObfuscatedName = (string)
+                                constructorArguments[3].Value;
+                        }
+                        else if (
+                            customAttribute.AttributeType.FullName
+                            == "net.runelite.mapping.ExportAttribute"
+                        )
+                        {
+                            CustomAttributeArgument[] constructorArguments =
+                                (CustomAttributeArgument[])
+                                    customAttribute.ConstructorArguments.First().Value;
+
+                            obfuscatedMethod.Export = (string)
+                                constructorArguments[3].Value;
+                        }
+                        else if (
+                            customAttribute.AttributeType.FullName
+                            == "net.runelite.mapping.ObfuscatedSignatureAttribute"
+                        )
+                        {
+                            CustomAttributeArgument[] constructorArguments =
+                                (CustomAttributeArgument[])
+                                    customAttribute.ConstructorArguments.First().Value;
+
+                            for (int i = 2; i < constructorArguments.Length; i++)
+                            {
+                                obfuscatedMethod.ObfuscatedSignature.Add(
+                                    (string)constructorArguments[i].Value
+                                );
+                            }
+                        }
+                        obfuscatedType.ObfuscatedMethods.Add(obfuscatedMethod);
+                    }
+                    obfuscatedTypes.Add(obfuscatedType);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
