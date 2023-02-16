52df528f1513a27ca1097c2628b6cd67fdb8eba7
Sean <scompton93@users.noreply.github.com>
2023-02-16 18:31:35 +1100

    refactor mixins - turn on render bypass

--- Mixins/GameEngine.cs
+++ Mixins/GameEngine.cs
@@ -12,7 +12,6 @@ namespace Mixins
         [MixinSettings(TargetClass = "GameEngine", TargetMethod = "paint")]
         public void paint(Graphics g)
         {
-            System.Console.WriteLine("Paint");
             if (this == GameEngine.gameEngine)
             {
                 if (!GameEngine.isKilled)
@@ -40,33 +39,28 @@ namespace Mixins
         [MixinSettings(TargetClass = "GameEngine", TargetMethod = "graphicsTick")]
         internal virtual void graphicsTick()
         {
-            System.Console.WriteLine("tick");
-
-
-
-            System.Console.WriteLine($"isCanvasInvalid = {isCanvasInvalid}");
-
-
             fullRedraw = true;
             resizeCanvasNextFrame = true;
 
+            System.Console.WriteLine("FPS: " + GameEngine.fps);
+
             Container container = this.container();
-            long num = global::class153.clockNow();
-            long num2 = global::GameEngine.graphicsTickTimes[global::class20.field69];
-            global::GameEngine.graphicsTickTimes[global::class20.field69] = num;
+            long clockNow = global::class153.clockNow();
+            long graphicsTickTimes = global::GameEngine.graphicsTickTimes[global::class20.field69];
+            global::GameEngine.graphicsTickTimes[global::class20.field69] = clockNow;
             global::class20.field69 = (global::class20.field69 + 1 & 31);
-            if (0L != num2 && num > num2)
+            if (0L != graphicsTickTimes && clockNow > graphicsTickTimes)
             {
-                int num3 = (int)(num - num2);
-                int num4 = (num3 >> 1) + 32000;
-                int num5 = num3;
+                int clockDiff = (int)(clockNow - graphicsTickTimes);
+                int num4 = (clockDiff >> 1) + 32000;
+                int num5 = clockDiff;
                 global::GameEngine.fps = ((num5 != -1) ? (num4 / num5) : (-num4));
             }
             if (++global::GameEngine.field129 - 1 > 50)
             {
                 global::GameEngine.field129 -= 50;
                 this.fullRedraw = true;
-                System.Threading.Thread.MemoryBarrier();
+
                 this.canvas.setSize(global::GameEngine.canvasWidth, global::class127.canvasHeight);
                 this.canvas.setVisible(true);
                 if (container == this.frame)
@@ -95,7 +89,6 @@ namespace Mixins
         [MixinSettings(TargetClass = "GameEngine", TargetMethod = "run")]
         public virtual void run()
         {
-            System.Console.WriteLine("Run");
             GameEngine.fiveOrOne = 5;
             this.setFocusCycleRoot(true);
             this.addCanvas();
@@ -103,12 +96,10 @@ namespace Mixins
             class177.clock = class161.method857();
             while (0L == GameEngine.stopTimeMs || class153.clockNow() < GameEngine.stopTimeMs)
             {
-                System.Console.WriteLine("Inside While");
                 InterfaceParent.gameCyclesToDo = class177.clock.wait(
                     GameEngine.cycleDurationMillis,
                     GameEngine.fiveOrOne
                 );
-                System.Console.WriteLine(InterfaceParent.gameCyclesToDo);
                 for (int i = 0; i < InterfaceParent.gameCyclesToDo; ++i)
                 {
                     this.clientTick();
@@ -119,17 +110,15 @@ namespace Mixins
 
             this.kill();
         }
+
         [MixinSettings(TargetClass = "GameEngine", TargetMethod = "clientTick")]
         void clientTick()
         {
-            System.Console.WriteLine("clientTick");
             long num = class153.clockNow();
             long num2 = GameEngine.clientTickTimes[ReflectionCheck.field172];
             GameEngine.clientTickTimes[ReflectionCheck.field172] = num;
             ReflectionCheck.field172 = (ReflectionCheck.field172 + 1 & 31);
-            if (0L == num2 || num > num2)
-            {
-            }
+            if (0L == num2 || num > num2) { }
             GameEngine.hasFocus = GameEngine.volatileFocus;
             this.doCycle();
         }
